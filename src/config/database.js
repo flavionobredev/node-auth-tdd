92fc9f9702a0a32b198b5eb7365189b33c82e647
Flavio Nobre <flavionobre11@gmail.com>
2021-08-29 17:27:29 -0300

    testing connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: "127.0.0.1",
+  host: "postgres-db",
   username: "docker",
   password: "docker",
   database: "nodeauth",
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,11 @@
 const routes = require("express").Router();
+const { User } = require("./app/models");
+
+User.create({
+  name: "Flavio",
+  email: "flavionobre@tallos.com",
+  password_hash: "dasdsakdakdasdkasd",
+});
 
 // definição de rotas
 
